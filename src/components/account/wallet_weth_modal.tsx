ecea22b7d4cf0ec5470980721a7f54aece9a48da
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-02-19 12:37:42 -0300

    (add) available balance modal styles

--- src/components/account/wallet_weth_modal.tsx
+++ src/components/account/wallet_weth_modal.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Modal from 'react-modal';
 import styled from 'styled-components';
 
+import { themeColors } from '../../util/theme';
 import { tokenAmountInUnits, unitsInTokenAmount } from '../../util/tokens';
 import { Button as ButtonBase } from '../common/button';
 
@@ -19,8 +20,8 @@ interface State {
 
 const Slider = styled.input`
     -webkit-appearance: none;
-    margin: 1em 0;
     cursor: pointer;
+    margin: 0 0 20px;
     width: 100%;
 
     &:focus {
@@ -28,71 +29,124 @@ const Slider = styled.input`
     }
 
     &::-webkit-slider-runnable-track {
-        height: 0.25em;
-
-        box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-        background: #3071a9;
+        background: ${themeColors.darkBlue};
         border-radius: 1.3px;
-        border: 0.2px solid #010101;
+        border: none;
+        height: 5px;
     }
 
     &::-webkit-slider-thumb {
         -webkit-appearance: none;
-        box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
-        border: 0.5px solid rgba(0, 0, 0, 0.142);
-        height: 1em;
-        width: 1em;
+        background: #fff;
         border-radius: 1em;
-        background: white;
+        border: 0.5px solid rgba(0, 0, 0, 0.142);
+        box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
         cursor: pointer;
-        margin-top: -0.4em;
+        height: 16px;
+        margin-top: -6px;
+        width: 16px;
     }
 `;
 
 const Button = styled(ButtonBase)`
     width: 100%;
-    font-weight: bold;
+`;
+
+const CloseButtonContainer = styled.div`
+    align-items: center;
+    display: flex;
+    height: 20px;
+    justify-content: flex-end;
 `;
 
 const CloseButton = styled.span`
-    float: right;
+    align-items: center;
     cursor: pointer;
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    margin-right: -10px;
+    width: 20px;
 `;
 
+const CloseButtonSVG = () => {
+    return (
+        <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M10.4501 10.449C10.7538 10.1453 10.7538 9.65282 10.4501 9.34909L6.60062 5.49996L10.45 1.65093C10.7537 1.3472 10.7537 0.854765 10.45 0.551038C10.1462 0.247311 9.65374 0.24731 9.34999 0.551038L5.50063 4.40006L1.65018 0.549939C1.34643 0.246212 0.853943 0.246212 0.55019 0.549939C0.246437 0.853667 0.246436 1.34611 0.55019 1.64983L4.40064 5.49996L0.550081 9.35019C0.246327 9.65392 0.246327 10.1464 0.550081 10.4501C0.853834 10.7538 1.34632 10.7538 1.65007 10.4501L5.50063 6.59985L9.3501 10.449C9.65385 10.7527 10.1463 10.7527 10.4501 10.449Z"
+                fill="#C4C4C4"
+            />
+        </svg>
+    );
+};
+
 const Title = styled.h1`
-    clear: both;
+    color: #000;
     font-size: 20px;
+    font-weight: 600;
+    line-height: 1.2;
+    margin: 0 0 25px;
+    text-align: center;
+`;
+
+const EthBoxes = styled.div`
+    column-gap: 16px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    margin-bottom: 25px;
+`;
+
+const EthBox = styled.div`
+    align-items: center;
+    border-radius: 4px;
+    border: 1px solid ${themeColors.borderColor};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    min-height: 105px;
+    padding: 10px;
+`;
+
+interface EthBoxProps {
+    isZero?: boolean;
+}
+
+const EthBoxValue = styled.h2<EthBoxProps>`
+    color: ${props => (props.isZero ? '#666' : themeColors.darkBlue)};
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 1.2;
+    margin: 0 0 8px;
+    text-align: center;
+`;
+
+const EthBoxUnit = styled.div`
+    color: ${themeColors.textLight};
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 1.2;
     text-align: center;
 `;
 
 const SetMinEthWrapper = styled.div`
-    margin-bottom: 1rem;
+    color: #666;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 1.2;
+    margin: 0 0 22px;
+    text-align: center;
 `;
 
 const SetMinEthButton = styled.a`
-    display: inline-block;
-    color: black;
+    color: #666;
     text-decoration: none;
     border-bottom: 1px dotted black;
 `;
 
-const EthBox = styled.div`
-    display: inline-block;
-    text-align: center;
-    border: 1px solid #dedede;
-    padding: 28px 40px;
-`;
-const WethBox = styled(EthBox)`
-    float: right;
-`;
-
-const EthBoxValue = styled.div`
-    font-size: 24px;
-    color: #666666;
-`;
-const EthBoxUnit = styled.div``;
-
 const minEth = unitsInTokenAmount('0.5', 18);
+const minSlidervalue = '0.00';
 
 class WethModal extends React.Component<Props, State> {
     public state = {
@@ -115,26 +169,30 @@ class WethModal extends React.Component<Props, State> {
 
         return (
             <Modal {...restProps}>
-                <CloseButton onClick={this.closeModal}>X</CloseButton>
+                <CloseButtonContainer>
+                    <CloseButton onClick={this.closeModal}>
+                        <CloseButtonSVG />
+                    </CloseButton>
+                </CloseButtonContainer>
                 <Title>Available Balance</Title>
-                <EthBox>
-                    <EthBoxValue>{selectedEthStr}</EthBoxValue>
-                    <EthBoxUnit>ETH</EthBoxUnit>
-                </EthBox>
-                <WethBox>
-                    <EthBoxValue>{selectedWethStr}</EthBoxValue>
-                    <EthBoxUnit>wETH</EthBoxUnit>
-                </WethBox>
-                <div>
-                    <Slider
-                        type="range"
-                        min="0"
-                        max={totalEthStr}
-                        step="0.01"
-                        value={selectedWethStr}
-                        onChange={this.updateSelectedWeth}
-                    />
-                </div>
+                <EthBoxes>
+                    <EthBox>
+                        <EthBoxValue isZero={selectedEthStr === minSlidervalue}>{selectedEthStr}</EthBoxValue>
+                        <EthBoxUnit>ETH</EthBoxUnit>
+                    </EthBox>
+                    <EthBox>
+                        <EthBoxValue isZero={selectedWethStr === minSlidervalue}>{selectedWethStr}</EthBoxValue>
+                        <EthBoxUnit>wETH</EthBoxUnit>
+                    </EthBox>
+                </EthBoxes>
+                <Slider
+                    max={totalEthStr}
+                    min="0"
+                    onChange={this.updateSelectedWeth}
+                    step="0.01"
+                    type="range"
+                    value={selectedWethStr}
+                />
                 {isInsufficientEth ? (
                     <SetMinEthWrapper>
                         ETH required for fees.&nbsp;
---
+++ src/util/theme.ts
@@ -0,0 +1,9 @@
+export const themeColors = {
+    orange: '#F6851B',
+    darkGray: '#474747',
+    darkBlue: '#002979',
+    green: '#3CB34F',
+    lightGray: '#B9B9B9',
+    textLight: '#999999',
+    borderColor: '#DEDEDE',
+};
