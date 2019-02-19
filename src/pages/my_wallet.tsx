e4617239a726d04e4e5fc55706619db806fad119
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-02-19 16:39:49 -0300

    (fix) linting

--- src/pages/my_wallet.tsx
+++ src/pages/my_wallet.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
+import { WalletTokenBalancesContainer, WalletWethBalanceContainer } from '../components/account';
 import { MainContent } from '../components/common/main_content';
 import { Sidebar } from '../components/common/sidebar';
-import { WalletTokenBalancesContainer, WalletWethBalanceContainer } from '../components/account';
 
 export const MyWallet = () => (
     <>
