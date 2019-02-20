393d559dd51e4f3e0d59f9465927ce92534612ef
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-02-20 16:34:46 -0300

    (remove) unused import

--- src/components/marketplace/order_book.tsx
+++ src/components/marketplace/order_book.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
 
 import { getOrderBook, getSelectedToken } from '../../store/selectors';
 import { themeColors } from '../../util/theme';
