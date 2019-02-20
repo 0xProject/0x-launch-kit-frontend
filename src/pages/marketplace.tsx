a4d29e835230120eebdd218b817f6961c737ead5
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-02-20 11:08:24 -0300

    (fix) linting

--- src/pages/marketplace.tsx
+++ src/pages/marketplace.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import { MainContent } from '../components/common/main_content';
 import { Sidebar } from '../components/common/sidebar';
