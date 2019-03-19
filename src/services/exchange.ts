b2845c3bf27074eef91d2f3082aa53691e07efa3
fvictorio <victorio.franco@gmail.com>
2019-03-19 15:56:10 -0300

    Fix use of fillEventCallback

--- src/services/exchange.ts
+++ src/services/exchange.ts
@@ -39,7 +39,7 @@ export const subscribeToFillEvents = async ({
         },
     );
 
-    pastFillEvents.forEach(x => fillEventCallback);
+    pastFillEvents.forEach(fillEventCallback);
 
     return subscription;
 };
