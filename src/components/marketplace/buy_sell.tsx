7871c656b9499b490e21aeffbc70337887499e4b
Agustin Pane <agustin.pane@gmail.com>
2019-03-19 16:57:31 -0300

    Replaces buy/sell button if the user does not have metamask installed or rejected the permissions

--- src/components/marketplace/buy_sell.tsx
+++ src/components/marketplace/buy_sell.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
-import { startBuySellLimitSteps, startBuySellMarketSteps } from '../../store/actions';
-import { getSelectedToken, getSelectedTokenSymbol } from '../../store/selectors';
+import { METAMASK_EXTENSION_URL } from '../../common/constants';
+import { connectWallet, startBuySellLimitSteps, startBuySellMarketSteps } from '../../store/actions';
+import { getSelectedToken, getSelectedTokenSymbol, getWeb3State } from '../../store/selectors';
+import { errorsWallet } from '../../util/error_messages';
 import { themeColors, themeDimensions } from '../../util/theme';
-import { OrderSide, StoreState, Token } from '../../util/types';
+import { OrderSide, StoreState, Token, Web3State } from '../../util/types';
 import { BigNumberInput } from '../common/big_number_input';
 import { Button } from '../common/button';
 import { CardBase } from '../common/card_base';
@@ -17,11 +19,13 @@ import { OrderDetails } from './order_details';
 interface StateProps {
     selectedTokenSymbol: string;
     selectedToken: Token | null;
+    web3State: Web3State;
 }
 
 interface DispatchProps {
     onSubmitLimitOrder: (amount: BigNumber, price: BigNumber, side: OrderSide) => Promise<any>;
     onSubmitMarketOrder: (amount: BigNumber, side: OrderSide) => Promise<any>;
+    onConnectWallet: () => any;
 }
 
 type Props = StateProps & DispatchProps;
@@ -146,6 +150,18 @@ const TokenTextButtonUppercase = styled.span`
     text-transform: uppercase;
 `;
 
+const WalletErrorText = styled.p`
+    font-size: 16px;
+    font-weight: normal;
+    line-height: 23px;
+    margin: 0;
+    padding: 20px 0;
+`;
+
+const ButtonStyled = styled(Button)`
+    width: 100%;
+`;
+
 class BuySell extends React.Component<Props, State> {
     public state = {
         makerAmount: new BigNumber(0),
@@ -170,6 +186,14 @@ class BuySell extends React.Component<Props, State> {
                 text: 'Limit',
             },
         ];
+
+        const errorBtn = this._getErrorBtn();
+        const buySellBtn = (
+            <Button theme="secondary" onClick={tab === OrderSide.Buy ? this.buy : this.sell}>
+                {tab === OrderSide.Buy ? 'Buy' : 'Sell'}{' '}
+                <TokenTextButtonUppercase>{selectedTokenSymbol}</TokenTextButtonUppercase>
+            </Button>
+        );
 
         return (
             <BuySellWrapper>
@@ -221,10 +245,7 @@ class BuySell extends React.Component<Props, State> {
                         tokenPrice={this.state.price}
                         selectedToken={this.props.selectedToken}
                     />
-                    <Button theme="secondary" onClick={tab === OrderSide.Buy ? this.buy : this.sell}>
-                        {tab === OrderSide.Buy ? 'Buy' : 'Sell'}{' '}
-                        <TokenTextButtonUppercase>{selectedTokenSymbol}</TokenTextButtonUppercase>
-                    </Button>
+                    {errorBtn ? errorBtn : buySellBtn}
                 </Content>
             </BuySellWrapper>
         );
@@ -260,6 +281,47 @@ class BuySell extends React.Component<Props, State> {
         this._reset();
     };
 
+    private readonly _getErrorBtn = () => {
+        const { onConnectWallet, web3State } = this.props;
+        let error;
+        switch (web3State) {
+            case Web3State.NotInstalled: {
+                error = (
+                    <>
+                        <WalletErrorText>Install Metamask wallet to make trades.</WalletErrorText>
+                        <ButtonStyled theme={'tertiary'} onClick={this._openMetamaskExtensionUrl}>
+                            {errorsWallet.mmGetExtension}
+                        </ButtonStyled>
+                    </>
+                );
+                break;
+            }
+            case Web3State.Locked: {
+                error = (
+                    <>
+                        <WalletErrorText>Accept metamask permissions in order to make trades.</WalletErrorText>
+                        <ButtonStyled theme={'tertiary'} onClick={onConnectWallet}>
+                            {errorsWallet.mmConnect}
+                        </ButtonStyled>
+                    </>
+                );
+                break;
+            }
+            default: {
+                error = null;
+                break;
+            }
+        }
+        return error;
+    };
+
+    private readonly _openMetamaskExtensionUrl = () => {
+        const win = window.open(METAMASK_EXTENSION_URL, '_blank');
+        if (win) {
+            win.focus();
+        }
+    };
+
     private readonly _reset = () => {
         this.setState({
             makerAmount: new BigNumber('0'),
@@ -284,6 +346,7 @@ const mapStateToProps = (state: StoreState): StateProps => {
     return {
         selectedTokenSymbol: getSelectedTokenSymbol(state),
         selectedToken: getSelectedToken(state),
+        web3State: getWeb3State(state),
     };
 };
 
@@ -292,6 +355,7 @@ const BuySellContainer = connect(
     {
         onSubmitLimitOrder: startBuySellLimitSteps,
         onSubmitMarketOrder: startBuySellMarketSteps,
+        onConnectWallet: connectWallet,
     },
 )(BuySell);
 
