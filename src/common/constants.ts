530cf26469b35d5b00dc273269dc198075ad85a4
Agustin Pane <agustin.pane@gmail.com>
2019-03-20 11:20:06 -0300

    Removed localhost default case for mainnet id

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -5,7 +5,7 @@ export const RELAYER_URL = process.env.REACT_APP_RELAYER_URL || 'http://localhos
 // Must be the same a as the relayer
 export const RELAYER_NETWORK_ID: number = Number.parseInt(process.env.REACT_APP_RELAYER_NETWORK_ID as string, 10) || 50;
 
-export const MAINNET_ID: number = Number.parseInt(process.env.REACT_APP_MAINNET_ID as string, 10) || 50;
+export const MAINNET_ID: number = Number.parseInt(process.env.REACT_APP_MAINNET_ID as string, 10) || 1;
 
 export const TX_DEFAULTS = {
     gasLimit: 1000000,
