5f6c7a0118e58292c2c6805450f7ebd447581211
Mariano Aguero <mariano.aguero@altoros.com>
2019-03-21 16:14:10 -0300

    Add notifications limit

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -45,3 +45,6 @@ export const METAMASK_EXTENSION_URL = 'https://metamask.io/';
 
 export const UPDATE_ETHER_PRICE_INTERVAL: number =
     Number.parseInt(process.env.REACT_APP_UPDATE_ETHER_PRICE_INTERVAL as string, 10) || 3600000;
+
+export const LIMIT_NUMBER_OF_NOTIFICATIONS: number =
+    Number.parseInt(process.env.REACT_APP_LIMIT_NUMBER_OF_NOTIFICATIONS as string, 10) || 20;
