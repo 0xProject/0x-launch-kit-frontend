23f40a9f183b4ff22940cc0a12421b186cc0852a
Agustin Pane <agustin.pane@gmail.com>
2019-04-01 12:27:01 -0300

    Moved getOrderSideFromFilledEvent to src/util/notifications Removed wethAssetData param from getOrderSideFromFilledEvent

--- src/util/notifications.ts
+++ src/util/notifications.ts
@@ -1,6 +1,6 @@
 import { assetDataUtils, ExchangeFillEventArgs, LogWithDecodedArgs } from '0x.js';
 
-import { getOrderSideFromFilledEvent, KnownTokens } from './known_tokens';
+import { KnownTokens } from './known_tokens';
 import { Market, NotificationKind, OrderFilledNotification, OrderSide, Token } from './types';
 
 export const buildOrderFilledNotification = (
@@ -9,11 +9,7 @@ export const buildOrderFilledNotification = (
     markets: Market[] | null,
 ): OrderFilledNotification => {
     const { args } = log;
-    const wethToken = knownTokens.getWethToken();
-
-    const wethAssetData = assetDataUtils.encodeERC20AssetData(wethToken.address);
-
-    const side: OrderSide = getOrderSideFromFilledEvent(knownTokens, log, markets, wethAssetData);
+    const side: OrderSide = getOrderSideFromFilledEvent(knownTokens, log, markets);
     let exchangedTokenAddress: string;
     let exchangedToken: Token;
     exchangedTokenAddress = OrderSide.Sell
@@ -29,4 +25,40 @@ export const buildOrderFilledNotification = (
         timestamp: new Date(),
         token: exchangedToken,
     };
+};
+
+export const getOrderSideFromFilledEvent = (
+    knownTokens: KnownTokens,
+    fillEvent: LogWithDecodedArgs<ExchangeFillEventArgs>,
+    markets: Market[] | null,
+): OrderSide => {
+    const { makerAssetData, takerAssetData } = fillEvent.args;
+    const wethToken = knownTokens.getWethToken();
+    const makerTokenAddress = assetDataUtils.decodeERC20AssetData(makerAssetData).tokenAddress;
+    const takerTokenAddress = assetDataUtils.decodeERC20AssetData(takerAssetData).tokenAddress;
+    const wethAssetData = assetDataUtils.encodeERC20AssetData(wethToken.address);
+    let orderSide: OrderSide = OrderSide.Sell;
+    if (!knownTokens.isValidFillEvent(fillEvent)) {
+        throw new Error('The event is not valid');
+    }
+    // Fallback in case there are not markets
+    if (!markets) {
+        orderSide = makerAssetData === wethAssetData ? OrderSide.Buy : OrderSide.Sell;
+    } else {
+        markets.forEach(market => {
+            const baseSymbol = market.currencyPair.base;
+            const quoteSymbol = market.currencyPair.quote;
+            const baseToken = knownTokens.getTokenBySymbol(baseSymbol);
+            const quoteToken = knownTokens.getTokenBySymbol(quoteSymbol);
+            if (makerTokenAddress === baseToken.address && takerTokenAddress === quoteToken.address) {
+                // This is a sell order --> fill event is a buy
+                orderSide = OrderSide.Buy;
+            } else if (makerTokenAddress === quoteToken.address && takerTokenAddress === baseToken.address) {
+                // This is a buy order --> fill event is a sell
+                orderSide = OrderSide.Sell;
+            }
+        });
+    }
+
+    return orderSide;
 };
