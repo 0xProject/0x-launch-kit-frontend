ca82f88ee8696cade728321c2df3487e747ee18e
Mariano Aguero <mariano.aguero@altoros.com>
2019-04-04 16:50:47 -0300

    Completed step text should remain dark

--- src/components/common/steps_modal/steps_progress.tsx
+++ src/components/common/steps_modal/steps_progress.tsx
@@ -102,11 +102,12 @@ export const StepsProgress: React.FC<Props> = props => {
     return (
         <StepsProgressWrapper {...restProps}>
             <StartingDot />
-            {steps.map((item, index) => {
+            {steps.map((item: StepItem, index) => {
+                const progress = +item.progress;
                 return (
                     <Step key={index}>
                         <StepLineContainer>
-                            <StepTitle active={item.active}>{item.title}</StepTitle>
+                            <StepTitle active={item.active || progress === 100}>{item.title}</StepTitle>
                             <StepLine>
                                 <StepLineProgress progress={item.progress} />
                             </StepLine>
