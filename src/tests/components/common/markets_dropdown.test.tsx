59fc48e560c578eb1541f1c816657859d3073b1d
Agustin Pane <agustin.pane@gmail.com>
2019-04-30 09:28:11 -0300

    Removed tests changes

--- src/tests/components/common/markets_dropdown.test.tsx
+++ src/tests/components/common/markets_dropdown.test.tsx
@@ -1,27 +1,13 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 
-import { MarketsDropdown } from '../../../components/common/markets_dropdown';
-import { tokenFactory } from '../../../util/test-utils';
-import { TokenSymbol } from '../../../util/types';
+import { MarketsDropdownContainer } from '../../../components/common/markets_dropdown';
 
 describe('Markets Dropdown', () => {
     let wrapper;
-    const noop = () => ({});
-    const currencyPair = {
-        base: TokenSymbol.Zrx,
-        quote: TokenSymbol.Weth,
-    };
+
     beforeEach(() => {
-        wrapper = shallow(
-            <MarketsDropdown
-                baseToken={tokenFactory.build()}
-                changeMarket={noop}
-                currencyPair={currencyPair}
-                goToHome={noop}
-                markets={null}
-            />,
-        );
+        wrapper = shallow(<MarketsDropdownContainer />);
     });
 
     it('Render the component', () => {
