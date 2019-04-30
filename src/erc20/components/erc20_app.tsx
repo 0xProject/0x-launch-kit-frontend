050a26ce3fc287ae714e0f444a341eb7911c9f7b
Jorge Shirai <jlshirai@gmail.com>
2019-04-30 17:13:40 -0300

    Use GeneralLayoutContainer in erc20 & erc721 apps.

--- src/erc20/components/erc20_app.tsx
+++ src/erc20/components/erc20_app.tsx
@@ -3,17 +3,17 @@ import { Route, Switch } from 'react-router';
 
 import { ERC20APP_BASE_PATH } from '../../common/constants';
 import { ToolbarContainer } from '../../components/common/toolbar';
-import { GeneralLayout } from '../../components/general_layout';
+import { GeneralLayoutContainer } from '../../components/general_layout';
 import { Marketplace } from '../pages/marketplace';
 import { MyWallet } from '../pages/my_wallet';
 
 const Toolbar = () => <ToolbarContainer />;
 
 export const Erc20App = () => (
-    <GeneralLayout renderToolbar={Toolbar}>
+    <GeneralLayoutContainer renderToolbar={Toolbar}>
         <Switch>
             <Route exact={true} path={`${ERC20APP_BASE_PATH}/`} component={Marketplace} />
             <Route exact={true} path={`${ERC20APP_BASE_PATH}/my-wallet`} component={MyWallet} />
         </Switch>
-    </GeneralLayout>
+    </GeneralLayoutContainer>
 );
--- src/erc721/components/erc721_app.tsx
+++ src/erc721/components/erc721_app.tsx
@@ -3,15 +3,15 @@ import { Route, Switch } from 'react-router';
 
 import { ERC721APP_BASE_PATH } from '../../common/constants';
 import { ToolbarContainer } from '../../components/common/toolbar';
-import { GeneralLayout } from '../../components/general_layout';
+import { GeneralLayoutContainer } from '../../components/general_layout';
 import { MyCollectibles } from '../pages/my_collectibles';
 
 const Toolbar = () => <ToolbarContainer />;
 
 export const Erc721App = () => (
-    <GeneralLayout renderToolbar={Toolbar}>
+    <GeneralLayoutContainer renderToolbar={Toolbar}>
         <Switch>
             <Route exact={true} path={`${ERC721APP_BASE_PATH}/`} component={MyCollectibles} />
         </Switch>
-    </GeneralLayout>
+    </GeneralLayoutContainer>
 );
