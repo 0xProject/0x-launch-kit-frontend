662867627114be21ab73a8e9fdfebd911857747f
Agustin Pane <agustin.pane@gmail.com>
2019-05-02 16:59:02 -0300

    Adjusted  input search styling

--- src/erc721/components/common/inputSearch.tsx
+++ src/erc721/components/common/inputSearch.tsx
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const StyledInputSearch = styled.div`
+const SearchIcon = styled.div`
     padding: 10px 10px;
     height: 52px;
     position: relative;
-
     &:before {
         content: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAQJJREFUKBWVkr2uQUEUhf3ET6GRaC5aFRoJKrf1BKpb8SwqovYGXkCj00k0QnRKEYkILYobvpUYmeMMyVnJl7P3mjN7Zu9zwiGv2qRFyMMSRrAFp6JPN8XzBj+wgDkUYAg7WINTYdwpDECxrRLJHeq2accdkgm8bzTvNAg2EDOGeUYI1KNO1gkuzTA1g8T7ojbn4ONQWPuHPWgeHmnzCqoe15tkSNPgPEAn68oVcOmA2XMtGK9FoE/VhOTTVNExqLCGZnxCv2pYauEC6lF0oQxX6IOvb7yX9NPEQafan+aPXDdQC18LsO6Tip5BBY6gIQaSbnMCFRCBZRcIvFkbsvCr4AFGOCxQy+JdGQAAAABJRU5ErkJggg==');
         display: block;
         position: absolute;
-        width: 15px;
-        z-index: 3;
-        height: 15px;
         font-size: 20px;
-        top: 11px;
-        left: 16px;
         line-height: 32px;
-        opacity: 0.6;
+        left: 12px;
+        width: 10px;
+        height: 10px;
+        padding-left: 10px;
+    }
+`;
+
+const SearchInput = styled.input`
+    background-color: #f9fafc;
+    text-align: center;
+    border: 1px solid #eaeaea;
+    box-sizing: border-box;
+    border-radius: 4px;
+    width: 468px;
+    height: 32px;
+
+    ::placeholder {
+        position: absolute;
+        left: 36.07%;
+        right: 17.36%;
+        top: 25%;
+        bottom: 9.38%;
+        font-family: Inter UI;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 14px;
+        line-height: 21px;
+        font-feature-settings: 'tnum' on, 'onum' on;
     }
 `;
 
@@ -25,9 +46,9 @@ export class Search extends Component {
     public render = () => {
         const { ...restProps } = this.props;
         return (
-            <StyledInputSearch>
-                <input {...restProps} placeholder={'Search Cryptocards'} />
-            </StyledInputSearch>
+            <SearchIcon>
+                <SearchInput {...restProps} placeholder={'Search Cryptocards'} />
+            </SearchIcon>
         );
     };
 }
