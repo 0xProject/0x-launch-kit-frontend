1791d9811b343d05475d8c15cf0c52a505dc84e1
Mariano Aguero <mariano.aguero@altoros.com>
2019-05-03 14:19:27 -0300

    Move my collectibles to my collectibles list

--- src/erc721/components/erc721_app.tsx
+++ src/erc721/components/erc721_app.tsx
@@ -5,7 +5,7 @@ import { ERC721_APP_BASE_PATH } from '../../common/constants';
 import { ToolbarErc721Container } from '../../components/common/toolbar_erc721';
 import { GeneralLayoutContainer } from '../../components/general_layout';
 import { Collectibles } from '../pages/collectibles';
-import { MyCollectiblesContainer } from '../pages/my_collectibles';
+import { MyCollectibles } from '../pages/my_collectibles';
 
 const Toolbar = <ToolbarErc721Container />;
 
@@ -13,7 +13,7 @@ export const Erc721App = () => (
     <GeneralLayoutContainer toolbar={Toolbar}>
         <Switch>
             <Route exact={true} path={`${ERC721_APP_BASE_PATH}/`} component={Collectibles} />
-            <Route exact={true} path={`${ERC721_APP_BASE_PATH}/my-collectibles`} component={MyCollectiblesContainer} />
+            <Route exact={true} path={`${ERC721_APP_BASE_PATH}/my-collectibles`} component={MyCollectibles} />
         </Switch>
     </GeneralLayoutContainer>
 );
--- src/erc721/pages/my_collectibles.tsx
+++ src/erc721/pages/my_collectibles.tsx
@@ -1,62 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import { CheckMetamaskStateModalContainer } from '../../components/common/check_metamask_state_modal_container';
-import { getMyCollectibles } from '../../store/selectors';
-import { Collectible, StoreState } from '../../util/types';
-import { CollectibleAsset } from '../components/collectible_asset';
-import { ColumnMyCollectibles } from '../components/column_my_collectibles';
+import { MyCollectiblesListContainer } from '../components/my_collectibles_list';
 
-interface Props {
-    myCollectibles: Collectible[];
-}
-
-class MyCollectibles extends React.Component<Props> {
-    public render = () => {
-        const { myCollectibles } = this.props;
-        const rows = myCollectibles.map((item, index) => {
-            const { name, price, image, color } = item;
-            return <CollectibleAsset name={name} price={price} image={image} color={color} key={index} />;
-        });
-
-        const groupSize = 2;
-        const rowsReduced = rows.reduce((row: any[], item, index) => {
-            const chunkIndex = Math.floor(index / groupSize);
-
-            if (!row[chunkIndex]) {
-                row[chunkIndex] = [] as any[];
-            }
-
-            row[chunkIndex].push(item);
-
-            return row;
-        }, []);
-
-        return (
-            <>
-                {rowsReduced.map((items, index) => {
-                    return (
-                        <React.Fragment key={index}>
-                            <ColumnMyCollectibles>
-                                {items.map((item: any) => {
-                                    return item;
-                                })}
-                            </ColumnMyCollectibles>
-                        </React.Fragment>
-                    );
-                })}
-                <CheckMetamaskStateModalContainer />
-            </>
-        );
-    };
-}
-
-const mapStateToProps = (state: StoreState): Props => {
-    return {
-        myCollectibles: getMyCollectibles(state),
-    };
-};
-
-const MyCollectiblesContainer = connect(mapStateToProps)(MyCollectibles);
-
-export { MyCollectibles, MyCollectiblesContainer };
+export const MyCollectibles = () => (
+    <>
+        <MyCollectiblesListContainer />
+        <CheckMetamaskStateModalContainer />
+    </>
+);
