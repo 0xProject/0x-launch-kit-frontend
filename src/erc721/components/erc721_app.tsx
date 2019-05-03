c3746ff235f642bd00e723b6c564b8d71ab48369
Mariano Aguero <mariano.aguero@altoros.com>
2019-05-03 10:32:16 -0300

    Move files

--- src/erc721/components/erc721_app.tsx
+++ src/erc721/components/erc721_app.tsx
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router';
 import { ERC721_APP_BASE_PATH } from '../../common/constants';
 import { ToolbarErc721Container } from '../../components/common/toolbar_erc721';
 import { GeneralLayoutContainer } from '../../components/general_layout';
-import { Collection } from '../pages/collection';
+import { Collectibles } from '../pages/collectibles';
 import { MyCollectibles } from '../pages/my_collectibles';
 
 const Toolbar = <ToolbarErc721Container />;
@@ -12,7 +12,7 @@ const Toolbar = <ToolbarErc721Container />;
 export const Erc721App = () => (
     <GeneralLayoutContainer toolbar={Toolbar}>
         <Switch>
-            <Route exact={true} path={`${ERC721_APP_BASE_PATH}/`} component={Collection} />
+            <Route exact={true} path={`${ERC721_APP_BASE_PATH}/`} component={Collectibles} />
             <Route exact={true} path={`${ERC721_APP_BASE_PATH}/my-collectibles`} component={MyCollectibles} />
         </Switch>
     </GeneralLayoutContainer>
