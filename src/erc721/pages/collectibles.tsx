fa3c7ce5a538ec167fe0fac31d6bc47287c886ed
Mariano Aguero <mariano.aguero@altoros.com>
2019-05-03 10:32:43 -0300

    Rename file to collectibles

--- src/erc721/pages/collectibles.tsx
+++ src/erc721/pages/collectibles.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { CheckMetamaskStateModalContainer } from '../../components/common/check_metamask_state_modal_container';
 
-export const Collection = () => (
+export const Collectibles = () => (
     <>
         <CheckMetamaskStateModalContainer />
         <pre>Collection page</pre>
