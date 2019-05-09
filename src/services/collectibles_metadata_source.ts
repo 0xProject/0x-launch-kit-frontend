2c1e14bb1fabf3555a04691634880c6739e7f33d
fvictorio <victorio.franco@gmail.com>
2019-05-09 17:53:07 -0300

    Make collectible price optional

--- src/services/collectibles_metadata_source.ts
+++ src/services/collectibles_metadata_source.ts
@@ -1,3 +1,5 @@
+import { BigNumber } from '0x.js';
+
 import { Collectible } from '../util/types';
 
 // @TODO: abstract the source of collectibles data (opensea/mocked)
@@ -5,7 +7,7 @@ const allCollectibles: Collectible[] = [
     {
         tokenId: '0',
         name: 'Glitter',
-        price: '2.30',
+        price: new BigNumber('2.30'),
         color: '#F6FEFC',
         image: 'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888670/6_w93q19.png',
         assetUrl: 'https://www.cryptokitties.co/',
@@ -15,7 +17,7 @@ const allCollectibles: Collectible[] = [
     {
         tokenId: '1',
         name: 'Furbeard',
-        price: '1.22',
+        price: null,
         color: '#F6C68A',
         image: 'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888668/9_xunbhn.png',
         assetUrl: 'https://www.cryptokitties.co/',
@@ -25,7 +27,7 @@ const allCollectibles: Collectible[] = [
     {
         tokenId: '2',
         name: 'Glasswalker',
-        price: '3.41',
+        price: new BigNumber('3.41'),
         color: '#CAFAF7',
         image: 'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888668/10_iqm4un.png',
         assetUrl: 'https://www.cryptokitties.co/',
@@ -35,7 +37,7 @@ const allCollectibles: Collectible[] = [
     {
         tokenId: '3',
         name: 'Ande',
-        price: '4.40',
+        price: null,
         color: '#B8F1B9',
         image: 'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888667/5_sxqrol.png',
         assetUrl: 'https://www.cryptokitties.co/',
@@ -45,7 +47,7 @@ const allCollectibles: Collectible[] = [
     {
         tokenId: '4',
         name: 'Squib',
-        price: '10.30',
+        price: null,
         color: '#CFD4F9',
         image: 'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888664/1_sz6sji.png',
         assetUrl: 'https://www.cryptokitties.co/',
@@ -55,7 +57,7 @@ const allCollectibles: Collectible[] = [
     {
         tokenId: '10',
         name: 'Negato',
-        price: '11.30',
+        price: null,
         color: '#D7BBF3',
         image: 'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888661/8_qjebni.png',
         assetUrl: 'https://www.cryptokitties.co/',
@@ -65,7 +67,7 @@ const allCollectibles: Collectible[] = [
     {
         tokenId: '11',
         name: 'DuCat',
-        price: '12.90',
+        price: new BigNumber('12.90'),
         color: '#D6DDD8',
         image: 'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888654/2_yndavu.png',
         assetUrl: 'https://www.cryptokitties.co/',
@@ -75,7 +77,7 @@ const allCollectibles: Collectible[] = [
     {
         tokenId: '12',
         name: 'Berry',
-        price: '2.30',
+        price: new BigNumber('2.30'),
         color: '#F7B4D5',
         image: 'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888653/4_do9hzd.png',
         assetUrl: 'https://www.cryptokitties.co/',
