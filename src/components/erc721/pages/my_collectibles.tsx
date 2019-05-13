dd953c0aa2ebae21b5cfeecd87eedc64339c67f4
Jorge Luis Shirai <jlshirai@gmail.com>
2019-05-13 14:43:52 -0300

    Update pages/all_collectibles file name.

--- src/components/erc721/pages/my_collectibles.tsx
+++ src/components/erc721/pages/my_collectibles.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import { CheckMetamaskStateModalContainer } from '../../common/check_metamask_state_modal_container';
 import { MyCollectiblesListContainer } from '../collectibles/my_collectibles_list';
 
 export const MyCollectibles = () => (
     <>
         <MyCollectiblesListContainer />
-        <CheckMetamaskStateModalContainer />
     </>
 );
