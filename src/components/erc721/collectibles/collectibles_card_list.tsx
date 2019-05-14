f5d23ca2fdbef19cf844a7e82b0140b4141f7b8a
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-05-14 14:51:29 -0300

    (update) home layout

--- src/components/erc721/collectibles/collectibles_card_list.tsx
+++ src/components/erc721/collectibles/collectibles_card_list.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { themeBreakPoints } from '../../../themes/commons';
 import { Collectible } from '../../../util/types';
 
 import { CollectibleAssetContainer } from './collectible_details';
 
-const CollectiblesListWrapper = styled.div``;
+const CollectiblesListOverflow = styled.div`
+    flex-grow: 1;
+    overflow: auto;
+`;
+
+const CollectiblesList = styled.div`
+    column-gap: 16px;
+    display: grid;
+    grid-template-columns: 1fr;
+    margin: 0 auto;
+    max-width: 1280px;
+    row-gap: 24px;
+    width: 100%;
+
+    @media (min-width: ${themeBreakPoints.md}) {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    @media (min-width: ${themeBreakPoints.xl}) {
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+
+    @media (min-width: ${themeBreakPoints.xxl}) {
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    }
+`;
 
 interface Props {
     collectibles: Collectible[];
@@ -14,21 +40,23 @@ interface Props {
 export const CollectiblesCardList = (props: Props) => {
     const { collectibles } = props;
     return (
-        <CollectiblesListWrapper>
-            {collectibles.map((item, index) => {
-                const { name, image, color, order, tokenId } = item;
-                const price = order ? order.takerAssetAmount : null;
-                return (
-                    <CollectibleAssetContainer
-                        name={name}
-                        price={price}
-                        image={image}
-                        color={color}
-                        id={tokenId}
-                        key={index}
-                    />
-                );
-            })}
-        </CollectiblesListWrapper>
+        <CollectiblesListOverflow>
+            <CollectiblesList>
+                {collectibles.map((item, index) => {
+                    const { name, image, color, order, tokenId } = item;
+                    const price = order ? order.takerAssetAmount : null;
+                    return (
+                        <CollectibleAssetContainer
+                            color={color}
+                            id={tokenId}
+                            image={image}
+                            key={index}
+                            name={name}
+                            price={price}
+                        />
+                    );
+                })}
+            </CollectiblesList>
+        </CollectiblesListOverflow>
     );
 };
