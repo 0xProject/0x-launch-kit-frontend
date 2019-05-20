98d456eae7576bcdb31ad164cf5f11e70a84799a
Agustin Pane <agustin.pane@gmail.com>
2019-05-20 13:29:32 -0300

    Now checks the complete amount of decimals in order to check if the empty value should be displayed or not

--- src/components/marketplace/order_book.tsx
+++ src/components/marketplace/order_book.tsx
@@ -48,10 +48,13 @@ const orderToRow = (
     }, new BigNumber(0));
 
     const mySizeConverted = tokenAmountInUnits(mySize, baseToken.decimals, UI_DECIMALS_DISPLAYED_ORDER_SIZE);
+    const zeroDecimals = '0'.repeat(UI_DECIMALS_DISPLAYED_ORDER_SIZE);
+    const mySizeStrDisplay = '0.'.concat(zeroDecimals);
+    const displayColor = mySizeConverted !== mySizeStrDisplay ? undefined : '#dedede';
     const mySizeRow =
         web3State !== Web3State.Locked && web3State !== Web3State.NotInstalled ? (
-            <CustomTD styles={{ tabular: true, textAlign: 'right' }}>
-                {mySizeConverted !== '0.00' ? mySizeConverted : '-'}
+            <CustomTD styles={{ tabular: true, textAlign: 'right', color: displayColor }}>
+                {mySizeConverted !== mySizeStrDisplay ? mySizeConverted : '-'}
             </CustomTD>
         ) : null;
 
