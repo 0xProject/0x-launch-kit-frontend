bdaf3fb94dc6428c913a66f3c7c973341ebb4adb
Mariano Aguero <mariano.aguero@altoros.com>
2019-05-21 10:30:57 -0300

    Add chart

--- src/components/erc721/marketplace/dutch_auction_price_chart_card.tsx
+++ src/components/erc721/marketplace/dutch_auction_price_chart_card.tsx
@@ -1,4 +1,6 @@
 import { BigNumber } from '0x.js';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -84,6 +86,62 @@ interface Props {
     collectible: Collectible;
 }
 
+interface ParamsPrices {
+    beginAmountInUnits: number;
+    endAmountInUnits: number;
+    currentPriceInUnits: number;
+}
+
+const generateMarkersByPrices = ({ beginAmountInUnits, endAmountInUnits, currentPriceInUnits }: ParamsPrices) => {
+    const dataNumbers: any[] = [];
+
+    let start = +beginAmountInUnits.toFixed(2);
+    const end = +endAmountInUnits.toFixed(2);
+    const currentPrice = +currentPriceInUnits.toFixed(2);
+
+    dataNumbers.push(start);
+    while (start > end) {
+        start = +(start - 0.01).toFixed(2);
+        dataNumbers.push(start);
+    }
+
+    // Lets add some marker, first Price marker
+    const indexCurrentPrice = dataNumbers.findIndex(element => {
+        return element === currentPrice;
+    });
+
+    if (indexCurrentPrice) {
+        dataNumbers[indexCurrentPrice] = {
+            y: dataNumbers[indexCurrentPrice],
+            marker: {
+                enabled: true,
+                symbol: 'circle',
+                radius: 8,
+            },
+        };
+    }
+
+    // Begin marker
+    dataNumbers[0] = {
+        y: dataNumbers[0],
+        marker: {
+            enabled: true,
+            symbol: 'circle',
+        },
+    };
+
+    // End marker
+    dataNumbers[dataNumbers.length - 1] = {
+        y: dataNumbers[dataNumbers.length - 1],
+        marker: {
+            enabled: true,
+            symbol: 'circle',
+        },
+    };
+
+    return dataNumbers;
+};
+
 export const DutchAuctionPriceChartCard = (props: Props) => {
     const { collectible } = props;
     const { order } = collectible;
@@ -95,30 +153,91 @@ export const DutchAuctionPriceChartCard = (props: Props) => {
     const { beginAmount, beginTimeSeconds } = getDutchAuctionData(makerAssetData);
     const price = getCollectiblePrice(collectible) as BigNumber;
     const { days, hours } = convertTimeInSecondsToDaysAndHours(expirationTimeSeconds.minus(beginTimeSeconds));
+
+    const currentPriceInUnits = +tokenAmountInUnits(price, ETH_DECIMALS);
+    const beginAmountInUnits = +tokenAmountInUnits(beginAmount, ETH_DECIMALS);
+    const endAmountInUnits = +tokenAmountInUnits(order.takerAssetAmount, ETH_DECIMALS);
+
+    const dataNumbers = generateMarkersByPrices({ beginAmountInUnits, endAmountInUnits, currentPriceInUnits });
+    const options = {
+        chart: {
+            type: 'area',
+            margin: [10, 5, 10, 10],
+            height: 148,
+            width: null,
+        },
+        title: {
+            text: '',
+        },
+        subtitle: {
+            text: '',
+        },
+        series: [
+            {
+                data: dataNumbers,
+                color: '#00AE99',
+                turboThreshold: dataNumbers.length,
+            } as Highcharts.SeriesColumnOptions,
+        ],
+        credits: {
+            enabled: false,
+        },
+        legend: {
+            enabled: false,
+        },
+        xAxis: {
+            title: {
+                text: '',
+            },
+            labels: {
+                enabled: false,
+            },
+            visible: false,
+        },
+        yAxis: {
+            title: {
+                text: '',
+            },
+            labels: {
+                enabled: false,
+            },
+            visible: false,
+            min: endAmountInUnits,
+            max: beginAmountInUnits,
+            startOnTick: false,
+            endOnTick: false,
+        },
+        tooltip: {
+            // prettier-ignore
+            formatter() {
+                const self: any = this;
+                return `Price: <b>${self.y} ETH</b>`;
+            }
+        },
+    };
+
     return (
         <Card>
             <CollectibleDescriptionInnerTitle>Price Chart</CollectibleDescriptionInnerTitle>
             <PriceChartContainer>
                 <PriceChartPriceAndTime>
                     <PriceChartTitle>Current Price</PriceChartTitle>
-                    <PriceChartValue>{tokenAmountInUnits(price, ETH_DECIMALS)} ETH</PriceChartValue>
+                    <PriceChartValue>{currentPriceInUnits} ETH</PriceChartValue>
                     <PriceChartTitle>Time Remaining</PriceChartTitle>
                     <PriceChartValue>{`${days} Days ${hours} Hrs`}</PriceChartValue>
                 </PriceChartPriceAndTime>
                 <PriceChartGraphWrapper>
-                    <PriceChartGraph />
+                    <PriceChartGraph>
+                        <HighchartsReact highcharts={Highcharts} options={options} />
+                    </PriceChartGraph>
                     <PriceChartGraphValues>
                         <div>
                             <PriceChartTitle>Start Price</PriceChartTitle>
-                            <PriceChartValueNeutral>
-                                {tokenAmountInUnits(beginAmount, ETH_DECIMALS)} ETH
-                            </PriceChartValueNeutral>
+                            <PriceChartValueNeutral>{beginAmountInUnits} ETH</PriceChartValueNeutral>
                         </div>
                         <div>
                             <PriceChartTitle>End Price</PriceChartTitle>
-                            <PriceChartValueNeutral>
-                                {tokenAmountInUnits(order.takerAssetAmount, ETH_DECIMALS)} ETH
-                            </PriceChartValueNeutral>
+                            <PriceChartValueNeutral>{endAmountInUnits} ETH</PriceChartValueNeutral>
                         </div>
                     </PriceChartGraphValues>
                 </PriceChartGraphWrapper>
