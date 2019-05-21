30bf2439b7e6b9f3fbe60d18335d35e1085c01df
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-05-21 16:37:44 -0300

    (add) collectibles list modal styles

--- src/components/erc721/collectibles/collectible_list_modal.tsx
+++ src/components/erc721/collectibles/collectible_list_modal.tsx
@@ -9,6 +9,7 @@ import { toggleCollectibleListModal } from '../../../store/ui/actions';
 import { Theme } from '../../../themes/commons';
 import { Collectible, StoreState } from '../../../util/types';
 import { CloseModalButton } from '../../common/icons/close_modal_button';
+import { CardLoading } from '../../common/loading';
 import { Search } from '../common/inputSearch';
 
 import { CollectibleOnListContainer } from './collectible_details_list';
@@ -33,17 +34,29 @@ interface State {
     currentCollectible: Collectible | null;
 }
 
+const modalWidth = '328px';
+const modalContentHeight = '400px';
+
 const ModalContent = styled.div`
-    width: 350px;
+    height: ${modalContentHeight};
+    margin-left: -${props => props.theme.modalTheme.content.padding};
+    margin-right: -${props => props.theme.modalTheme.content.padding};
+    margin-top: -${props => props.theme.modalTheme.content.padding};
+    overflow: auto;
+    width: 360px;
 `;
 
 const ModalTitleWrapper = styled.div`
-    align-items: center;
     border-bottom: 1px solid ${props => props.theme.componentsTheme.borderColor};
+    margin: -3px -${props => props.theme.modalTheme.content.padding} 15px;
+    padding: 0 ${props => props.theme.modalTheme.content.padding} 13px;
+`;
+
+const ModalTitleTop = styled.div`
+    align-items: center;
     display: flex;
     justify-content: space-between;
-    margin: -3px -16px 15px;
-    padding: 0 16px 13px;
+    margin-bottom: 12px;
 `;
 
 const ModalTitle = styled.h2`
@@ -53,6 +66,18 @@ const ModalTitle = styled.h2`
     line-height: 1.2;
     margin: 0;
     padding: 0 15px 0 0;
+`;
+
+const SearchStyled = styled(Search)`
+    background-color: ${props => props.theme.componentsTheme.marketsSearchFieldBackgroundColor};
+    border-color: ${props => props.theme.componentsTheme.marketsSearchFieldBackgroundColor};
+    color: ${props => props.theme.componentsTheme.marketsSearchFieldTextColor};
+    max-width: 100%;
+    width: ${modalWidth};
+
+    ::placeholder {
+        text-align: left;
+    }
 `;
 
 const CloseModalButtonStyle = styled(CloseModalButton)`
@@ -76,20 +101,27 @@ class CollectibleListModalContainer extends React.Component<Props> {
         const collectible = userCollectibles[0];
         if (collectible) {
             content = collectibles.map((item, index) => {
-                return <CollectibleOnListContainer collectible={item} onClick={this._closeModal} key={index} />;
+                return (
+                    <CollectibleOnListContainer
+                        collectible={item}
+                        isListItem={true}
+                        key={index}
+                        onClick={this._closeModal}
+                    />
+                );
             });
         }
 
         return (
             <Modal isOpen={isCollectibleModalOpen} style={theme.modalTheme} onRequestClose={this._closeModal}>
                 <ModalTitleWrapper>
-                    <ModalTitle>Selling Item</ModalTitle>
-                    <CloseModalButtonStyle onClick={this._closeModal} />
+                    <ModalTitleTop>
+                        <ModalTitle>Selling Item</ModalTitle>
+                        <CloseModalButtonStyle onClick={this._closeModal} />
+                    </ModalTitleTop>
+                    <SearchStyled placeHolder={'Search Wallet'} />
                 </ModalTitleWrapper>
-                <ModalContent>
-                    <Search placeHolder={'Search Wallet'} />
-                    {content}
-                </ModalContent>
+                <ModalContent>{content ? content : <CardLoading minHeight={modalContentHeight} />}</ModalContent>
             </Modal>
         );
     };
