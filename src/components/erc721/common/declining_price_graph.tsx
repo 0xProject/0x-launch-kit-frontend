e92af49230c0d17669a66c714fb6c5ef91e2fb26
Mariano Aguero <mariano.aguero@altoros.com>
2019-05-22 12:30:55 -0300

    Update to basic component, update chart when currentPrice is changed

--- src/components/erc721/common/declining_price_graph.tsx
+++ src/components/erc721/common/declining_price_graph.tsx
@@ -8,60 +8,8 @@ interface Props {
     currentPriceInUnits: number;
 }
 
-const generateMarkersByPrices = ({ beginAmountInUnits, endAmountInUnits, currentPriceInUnits }: Props) => {
-    const dataNumbers: any[] = [];
-
-    let start = +beginAmountInUnits.toFixed(2);
-    const end = +endAmountInUnits.toFixed(2);
-    const currentPrice = +currentPriceInUnits.toFixed(2);
-
-    dataNumbers.push(start);
-    while (start > end) {
-        start = +(start - 0.01).toFixed(2);
-        dataNumbers.push(start);
-    }
-
-    // Lets add some marker, first Price marker
-    const indexCurrentPrice = dataNumbers.findIndex(element => {
-        return element === currentPrice;
-    });
-
-    if (indexCurrentPrice) {
-        dataNumbers[indexCurrentPrice] = {
-            y: dataNumbers[indexCurrentPrice],
-            marker: {
-                enabled: true,
-                symbol: 'circle',
-                radius: 8,
-            },
-        };
-    }
-
-    // Begin marker
-    dataNumbers[0] = {
-        y: dataNumbers[0],
-        marker: {
-            enabled: true,
-            symbol: 'circle',
-        },
-    };
-
-    // End marker
-    dataNumbers[dataNumbers.length - 1] = {
-        y: dataNumbers[dataNumbers.length - 1],
-        marker: {
-            enabled: true,
-            symbol: 'circle',
-        },
-    };
-
-    return dataNumbers;
-};
-
-export const DecliningPriceGraph = ({ beginAmountInUnits, endAmountInUnits, currentPriceInUnits }: Props) => {
-    const dataNumbers = generateMarkersByPrices({ beginAmountInUnits, endAmountInUnits, currentPriceInUnits });
-
-    const options = {
+const initialState = {
+    options: {
         chart: {
             type: 'area',
             margin: [10, 5, 10, 10],
@@ -74,21 +22,13 @@ export const DecliningPriceGraph = ({ beginAmountInUnits, endAmountInUnits, curr
         subtitle: {
             text: '',
         },
-        // tslint:disable
-        series: [
-            {
-                data: dataNumbers,
-                color: '#00AE99',
-                turboThreshold: dataNumbers.length,
-            } as Highcharts.SeriesColumnOptions,
-        ],
-        // tslint:enable
         credits: {
             enabled: false,
         },
         legend: {
             enabled: false,
         },
+        series: [],
         xAxis: {
             title: {
                 text: '',
@@ -97,19 +37,6 @@ export const DecliningPriceGraph = ({ beginAmountInUnits, endAmountInUnits, curr
                 enabled: false,
             },
             visible: false,
-        },
-        yAxis: {
-            title: {
-                text: '',
-            },
-            labels: {
-                enabled: false,
-            },
-            visible: false,
-            min: endAmountInUnits,
-            max: beginAmountInUnits,
-            startOnTick: false,
-            endOnTick: false,
         },
         tooltip: {
             // tslint:disable
@@ -120,7 +47,117 @@ export const DecliningPriceGraph = ({ beginAmountInUnits, endAmountInUnits, curr
             }
             // tslint:enable
         },
+    },
+};
+
+class DecliningPriceGraph extends React.Component<Props> {
+    public state: any = {
+        ...initialState,
     };
 
-    return <HighchartsReact highcharts={Highcharts} options={options} />;
-};
+    public componentDidMount = () => {
+        this._generateMarkersWithPrices();
+    };
+
+    public componentDidUpdate = (prevProps: Readonly<Props>) => {
+        const { currentPriceInUnits } = this.props;
+        if (prevProps.currentPriceInUnits !== currentPriceInUnits) {
+            this._generateMarkersWithPrices();
+        }
+    };
+
+    public render = () => {
+        return <HighchartsReact highcharts={Highcharts} options={this.state.options} />;
+    };
+
+    private readonly _generateMarkersWithPrices = () => {
+        const { beginAmountInUnits, endAmountInUnits, currentPriceInUnits } = this.props;
+
+        let start = +beginAmountInUnits.toFixed(2);
+        const end = +endAmountInUnits.toFixed(2);
+        const currentPrice = +currentPriceInUnits.toFixed(2);
+
+        const dataNumbers: any[] = [
+            {
+                y: start,
+                marker: {
+                    enabled: false,
+                },
+            },
+        ];
+
+        while (start > end) {
+            start = +(start - 0.01).toFixed(2);
+            dataNumbers.push({
+                y: start,
+                marker: {
+                    enabled: false,
+                },
+            });
+        }
+
+        // Lets add some marker, first Price marker
+        const indexCurrentPrice = dataNumbers.findIndex(element => {
+            return element.y === currentPrice;
+        });
+
+        if (indexCurrentPrice) {
+            dataNumbers[indexCurrentPrice] = {
+                y: dataNumbers[indexCurrentPrice].y,
+                marker: {
+                    enabled: true,
+                    symbol: 'circle',
+                    radius: 8,
+                },
+            };
+        }
+
+        // Begin marker
+        dataNumbers[0] = {
+            y: dataNumbers[0].y,
+            marker: {
+                enabled: true,
+                symbol: 'circle',
+            },
+        };
+
+        // End marker
+        dataNumbers[dataNumbers.length - 1] = {
+            y: dataNumbers[dataNumbers.length - 1].y,
+            marker: {
+                enabled: true,
+                symbol: 'circle',
+            },
+        };
+
+        this.setState({
+            options: {
+                ...this.state.options,
+                // tslint:disable
+                series: [
+                    {
+                        data: dataNumbers,
+                        color: '#00AE99',
+                        turboThreshold: dataNumbers.length,
+                    } as Highcharts.SeriesColumnOptions,
+                ],
+                // tslint:enable
+                yAxis: {
+                    title: {
+                        text: '',
+                    },
+                    labels: {
+                        enabled: false,
+                    },
+                    visible: false,
+                    min: endAmountInUnits,
+                    max: beginAmountInUnits,
+                    startOnTick: false,
+                    endOnTick: false,
+                },
+            },
+        });
+    };
+}
+
+export { DecliningPriceGraph };
