2f4e86029c76e394cd17af50413d1fce8334fdbc
Agustin Pane <agustin.pane@gmail.com>
2019-05-27 16:46:11 -0300

    Makes token balance symbol uppercase

--- src/exceptions/insufficient_token_balance_exception.ts
+++ src/exceptions/insufficient_token_balance_exception.ts
@@ -2,6 +2,6 @@ import { TokenSymbol } from '../util/types';
 
 export class InsufficientTokenBalanceException extends Error {
     constructor(quoteSymbol: TokenSymbol) {
-        super(`You don't have enough ${quoteSymbol}...`);
+        super(`You don't have enough ${quoteSymbol.toUpperCase()}...`);
     }
 }
