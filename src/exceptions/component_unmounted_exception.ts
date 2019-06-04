7de9195005013e5aa2adaefa888d40a1f9266e75
Jorge Luis Shirai <jlshirai@gmail.com>
2019-06-04 12:33:40 -0300

    Move steps_modal/exceptions to src/exceptions.
    Rename unmounted_exception and made it more general.

--- src/exceptions/component_unmounted_exception.ts
+++ src/exceptions/component_unmounted_exception.ts
@@ -1,6 +1,6 @@
-export class BaseStepModalUnmountedException extends Error {
-    constructor() {
-        super('BaseStepModal unmounted');
+export class ComponentUnmountedException extends Error {
+    constructor(componentName: string) {
+        super(`${componentName} unmounted`);
         // see: typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html
         Object.setPrototypeOf(this, new.target.prototype);
     }
