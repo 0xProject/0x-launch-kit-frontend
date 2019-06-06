5142570954b0c8c0b7cbfed1f4d61c81e164ee1d
Jorge Luis Shirai <jlshirai@gmail.com>
2019-06-06 10:53:07 -0300

    Use constant in user_denied_transaction_exception.
    Change src/exceptions/common.tsx extension to .ts.

--- src/exceptions/user_denied_transaction_exception.ts
+++ src/exceptions/user_denied_transaction_exception.ts
@@ -1,5 +1,7 @@
+import { USER_DENIED_TRANSACTION_SIGNATURE_ERR } from './common';
+
 export class UserDeniedTransactionSignatureException extends Error {
-    constructor(m: string = 'User denied transaction signature') {
+    constructor(m: string = USER_DENIED_TRANSACTION_SIGNATURE_ERR) {
         super(m);
         // Set the prototype explicitly.
         Object.setPrototypeOf(this, UserDeniedTransactionSignatureException.prototype);
